24b7758677253971c8cf4635ba8e3add478e325c
Dan Mills <hello@sandmill.org>
2018-06-25 22:07:50 -0700

    add blank db seeder

---
+++ database/seeds/DatabaseSeeder.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| DatabaseSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+const Factory = use('Factory')
+
+class DatabaseSeeder {
+  async run () {
+  }
+}
+
+module.exports = DatabaseSeeder
