b7740301be24cf9de3ce1ae03419df5418f2ebb2
Dan Mills <hello@sandmill.org>
2018-06-25 22:56:18 -0700

    add calendar events db schema

---
+++ database/migrations/1529992301232_calendar_events_schema.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class CalendarEventsSchema extends Schema {
+  up () {
+    this.create('calendar_events', (table) => {
+      table.increments()
+      table.string('title', 254).notNullable()
+      table.boolean('is_all_day').defaultsTo(false)
+      table.datetime('start').notNullable()
+      table.datetime('end')
+      table.text('description')
+      table.string('category', 80).notNullable().defaultsTo('class') // class, meetup, talk, private
+      table.integer('price').defaultsTo(0)
+      table.integer('member_price')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('calendar_events')
+  }
+}
+
+module.exports = CalendarEventsSchema
