4fe092b50b06267013599283cababbda44d97de9
Dan Mills <hello@sandmill.org>
2018-07-03 12:37:57 -0700

    add camelCase computed properties to Product so they'll be serialized to JSON (to match our GraphQL Schema)

--- app/Models/Product.js
+++ app/Models/Product.js
@@ -6,6 +6,40 @@ class Product extends Model {
   categories () {
     return this.belongsToMany('App/Models/Category')
   }
+
+  static get computed() {
+    return ['billingDescription', 'memberPrice', 'bundledUnits',
+            'isSubscription', 'subscriptionPeriod'
+            'subscriptionName', 'subscriptionPlan'];
+  }
+
+  getBillingDescription() {
+    return this.billing_description;
+  }
+
+  getMemberPrice() {
+    return this.member_price;
+  }
+
+  getBundledUnits() {
+    return this.bundled_units;
+  }
+
+  getIsSubscription() {
+    return this.is_subscription;
+  }
+
+  getSubscriptionPeriod() {
+    return this.subscription_period;
+  }
+
+  getSubscriptionName() {
+    return this.subscription_name;
+  }
+
+  getSubscriptionPlan() {
+    return this.subscription_plan;
+  }
 }
 
 module.exports = Product
