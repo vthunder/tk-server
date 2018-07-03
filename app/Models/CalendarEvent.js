0cb59adb622e42eb255aaea75d145e15c555a7aa
Dan Mills <hello@sandmill.org>
2018-07-03 12:39:34 -0700

    add camelCase properties to CalendarEvents, and format start/end dates as YYYY-MM-DD HH:mm:ss

--- app/Models/CalendarEvent.js
+++ app/Models/CalendarEvent.js
@@ -1,8 +1,27 @@
 'use strict'
 
 const Model = use('Model')
+const moment = use('moment')
 
 class CalendarEvent extends Model {
+  // camelcase props for json -> graphql api
+  static get computed() {
+    return ['allDay', 'memberPrice'];
+  }
+  getAllDay() {
+    return this.is_all_day;
+  }
+  getMemberPrice() {
+    return this.member_price;
+  }
+
+  getStart() {
+    return moment(this.start).format('YYYY-MM-DD HH:mm:ss');
+  }
+
+  getEnd() {
+    return moment(this.start).format('YYYY-MM-DD HH:mm:ss');
+  }
 }
 
 module.exports = CalendarEvent
