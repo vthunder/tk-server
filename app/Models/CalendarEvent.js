e134848cf40c347c97b01e3be94ab6b88cf45d25
Dan Mills <hello@sandmill.org>
2018-07-09 22:26:04 -0700

    add computed 'end' getter calculated from start+is_all_day+duration

--- app/Models/CalendarEvent.js
+++ app/Models/CalendarEvent.js
@@ -6,21 +6,29 @@ const moment = use('moment')
 class CalendarEvent extends Model {
   // camelcase props for json -> graphql api
   static get computed() {
-    return ['allDay', 'memberPrice'];
+    return ['allDay', 'end', 'memberPrice'];
   }
+
   getAllDay() {
-    return this.is_all_day;
-  }
-  getMemberPrice() {
-    return this.member_price;
+    return this.is_all_day
   }
 
   getStart() {
-    return moment(this.start).format('YYYY-MM-DD HH:mm:ss');
+    return moment(this.start).format('YYYY-MM-DD HH:mm:ss')
   }
 
   getEnd() {
-    return moment(this.start).format('YYYY-MM-DD HH:mm:ss');
+    if (this.is_all_day) {
+      return null
+    } else {
+      return moment(this.start)
+        .add(this.duration, 'hours')
+        .format('YYYY-MM-DD HH:mm:ss')
+    }
+  }
+
+  getMemberPrice() {
+    return this.member_price;
   }
 }
 
