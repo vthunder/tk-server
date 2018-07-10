07cc3a772713bc91799312fce79e120549fe0a90
Dan Mills <hello@sandmill.org>
2018-07-09 23:06:49 -0700

    seed dan user and membership products

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -11,18 +11,54 @@
 */
 
 const Factory = use('Factory')
+const User = use('App/Models/User')
+const Product = use('App/Models/Product')
 
 class DatabaseSeeder {
   async run () {
+    let user = new User()
+    user.name = 'dan'
+    user.email = 'danmills@sandmill.org'
+    user.password = 'sheepish alliance yarmouth flub'
+    await user.save()
+
     const usersArray = await Factory
           .model('App/Models/User')
           .createMany(5);
 
+    let product = new Product()
+    product.id = 1
+    product.title = 'Membership (Monthly)'
+    product.description = 'Get all membership benefits, billed monthly'
+    product.billing_description = 'Tinker Kitchen membership (monthly)'
+    product.price = 15000
+    product.member_price = null
+    product.bundled_units = 1
+    product.unit = 'month'
+    product.is_subscription = true
+    product.subscription_period = 'monthly'
+    product.subscription_name = 'membership'
+    product.subscription_plan = 'membership-monthly'
+    await product.save()
+
+    product = new Product()
+    product.id = 2
+    product.title = 'Membership (Yearly)'
+    product.description = 'Get all membership benefits, billed yearly'
+    product.billing_description = 'Tinker Kitchen membership (yearly)'
+    product.price = 150000
+    product.member_price = null
+    product.bundled_units = 12
+    product.unit = 'month'
+    product.is_subscription = true
+    product.subscription_period = 'yearly'
+    product.subscription_name = 'membership'
+    product.subscription_plan = 'membership-yearly'
+    await product.save()
+
     const eventsArray = await Factory
           .model('App/Models/CalendarEvent')
           .createMany(15);
-
-
   }
 }
 
