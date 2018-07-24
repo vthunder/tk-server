9ab3b2e1875cf1552714dc733f07211ecd4a3be1
Dan Mills <hello@sandmill.org>
2018-07-23 23:17:41 -0700

    implement is_member and membership_sub in user model

--- app/Models/User.js
+++ app/Models/User.js
@@ -8,6 +8,10 @@ class User extends Model {
     this.addHook('beforeSave', 'UserHook.hashPassword')
   }
 
+  static get computed() {
+    return ['is_member', 'membership_sub']
+  }
+
   /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
@@ -32,6 +36,21 @@ class User extends Model {
 
   creditCards () {
     return this.hasMany('App/Models/CreditCard')
+  }
+
+  getIsMember() {
+    if (this.getMembershipSub()) return true
+    return false
+  }
+
+  async getMembershipSub() {
+    let sub
+    await this.subscriptions().forEach((s) => {
+      if (s.name === 'membership') {
+        sub = s
+      }
+    })
+    return sub
   }
 
   static get traits() {
