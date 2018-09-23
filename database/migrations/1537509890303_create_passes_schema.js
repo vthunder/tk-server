913244c787984157d69e26baf32a8a80ed74c128
Dan Mills <hello@sandmill.org>
2018-09-23 01:24:44 -0700

    Add passes table to store day passes belonging to users

---
+++ database/migrations/1537509890303_create_passes_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class PassesSchema extends Schema {
+  up () {
+    this.create('passes', (table) => {
+      table.increments()
+      table.string('token').unique()
+      table.string('type').defaultsTo('day_pass')
+      table.string('order_id') // Stripe object ID
+      table.integer('user_id').unsigned().index()
+      table.foreign('user_id').references('id').on('users').onDelete('cascade')
+      table.string('status').defaultsTo('new') // 'used' once consumed
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('passes')
+  }
+}
+
+module.exports = PassesSchema
