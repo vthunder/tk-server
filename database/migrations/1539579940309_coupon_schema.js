b422ed143ca3025af157d66b58c002149c4b2dbe
Dan Mills <hello@sandmill.org>
2018-10-19 15:01:49 -0700

    change coupon type also carry the source/reason now, so accordingly: change default type to 'ks_month' ('ks' -> kickstarter, 'month' -> '1 month membership'). different coupon types can have different reasons for being or confer different benefits.

--- database/migrations/1539579940309_coupon_schema.js
+++ database/migrations/1539579940309_coupon_schema.js
@@ -6,7 +6,7 @@ class CouponTokenSchema extends Schema {
   up () {
     this.create('coupon_tokens', (table) => {
       table.increments()
-      table.string('type').notNullable().defaultTo('monthly_member') // or yearly_member
+      table.string('type').notNullable().defaultTo('ks_month') // or ks_year, staff, ...
       table.string('token').notNullable()
       table.string('status').notNullable().defaultTo('new') // or used
       table.integer('claimed_by')
