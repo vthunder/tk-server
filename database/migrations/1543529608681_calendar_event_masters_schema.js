23d4c8c49d8c2baf5baf64296f218907a67a542a
Dan Mills <hello@sandmill.org>
2018-12-20 22:59:06 -0800

    add 'featured' flag to show masters in class list even if not scheduled; add slug

--- database/migrations/1543529608681_calendar_event_masters_schema.js
+++ database/migrations/1543529608681_calendar_event_masters_schema.js
@@ -7,9 +7,9 @@ class CalendarEventMastersSchema extends Schema {
     this.create('calendar_event_masters', (table) => {
       table.increments()
       table.string('sku_id')
-      table.string('member_sku_id')
-      table.string('member_discount_id')
+      table.boolean('featured').defaultsTo(false)
       table.string('title', 254).notNullable()
+      table.string('slug', 254).notNullable()
       table.string('image_header', 254).defaultsTo('/images/default-class-header.jpg')
       table.boolean('is_all_day').defaultsTo(false)
       table.float('duration')
@@ -17,21 +17,21 @@ class CalendarEventMastersSchema extends Schema {
       table.string('category', 80).notNullable().defaultsTo('class') // class, meetup, talk, private
       table.integer('price')
       table.integer('member_price')
+      table.integer('max_size').defaultsTo(10)
       table.string('ext_book_url')
       table.string('ext_member_discount_code')
-      table.integer('max_size').defaultsTo(20)
       table.timestamps()
     })
     this.alter('calendar_events', (table) => {
-      table.integer('calendar_event_master_id').unsigned().index()
-      table.foreign('calendar_event_master_id').references('id').on('calendar_event_masters').onDelete('cascade')
+      table.integer('master_id').unsigned().index()
+      table.foreign('master_id').references('id').on('calendar_event_masters').onDelete('cascade')
     })
   }
 
   down () {
     this.alter('calendar_events', (table) => {
-      table.dropForeign('calendar_event_master_id')
-      table.dropColumn('calendar_event_master_id')
+      table.dropForeign('master_id')
+      table.dropColumn('master_id')
     })
     this.drop('calendar_event_masters')
   }
