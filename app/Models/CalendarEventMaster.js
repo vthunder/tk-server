9ea2c461b4b479cc9d94aa905858aee932eba71a
Dan Mills <hello@sandmill.org>
2018-12-20 22:54:31 -0800

    serialize array of events belonging to this master

--- app/Models/CalendarEventMaster.js
+++ app/Models/CalendarEventMaster.js
@@ -2,6 +2,32 @@
 
 const Model = use('Model')
 
-class CalendarEventMaster extends Model {}
+class CalendarEventMaster extends Model {
+  static get computed() {
+    return ['events']
+  }
+
+  events() {
+    return this.hasMany('App/Models/CalendarEvent', 'id', 'master_id')
+  }
+
+  async load_events() {
+    const events = await this.events().fetch()
+    this._events = events.rows.map(async (e) => {
+      await e.load_master()
+      return {
+        id: e.id,
+        start: e.getStart(),
+        duration: e.getDuration(),
+        all_day: e.getAllDay(),
+        sku_id: e.sku_id,
+      }
+    })
+  }
+
+  getEvents() {
+    return this._events || []
+  }
+}
 
 module.exports = CalendarEventMaster
