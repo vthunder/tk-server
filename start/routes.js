3a483731b843520d13ce3ab7a89c48da1c69d047
Dan Mills <hello@sandmill.org>
2019-06-05 19:10:35 -0700

    add another calendar for when tk is closed only

--- start/routes.js
+++ start/routes.js
@@ -53,3 +53,34 @@ Route.route('/ical', async (context) => {
     events: processed_events,
   }).toString();
 }, ['GET'])
+
+Route.route('/ical/closed', async (context) => {
+  let events = await CalendarEvent.query()
+      .where('category', '=', 'private')
+      .orWhere('category', '=', 'special')
+      .fetch()
+  const masters = await CalendarEventMaster.all()
+  const default_master_id = Config.get('app.default_event_master_id')
+
+  for (let n = 0; n < events.rows.length; n++) {
+    const id = events.rows[n].master_id || default_master_id
+    const master = masters.rows.filter(m => m.id === id)
+    events.rows[n].master = master[0]
+  }
+
+  let processed_events = events.toJSON().filter(e => !e.calendar_hide).map((e) => {
+    delete e.status
+    e.summary = e.title
+    // FIXME: gross hack because I cannot figure out proper timezone support
+    e.start = moment(e.start).subtract(1, 'hour').format()
+    e.end = moment(e.end).subtract(1, 'hour').format()
+    return e
+  })
+
+  return ical({
+    name: 'Tinker Kitchen - Closed',
+    domain: 'tinkerkitchen.org',
+    timezone: 'America/Los_Angeles',
+    events: processed_events,
+  }).toString();
+}, ['GET'])
