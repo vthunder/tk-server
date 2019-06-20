cc8e3f384d98db2d842b475c3f6651d7dfa494c2
Dan Mills <hello@sandmill.org>
2019-06-20 12:28:22 -0700

    reorder db fields

--- database/migrations/1560989167572_qr_scan_log_schema.js
+++ database/migrations/1560989167572_qr_scan_log_schema.js
@@ -5,10 +5,10 @@ const Schema = use('Schema')
 class QrScanLogSchema extends Schema {
   up () {
     this.create('qr_scan_logs', (table) => {
+      table.increments()
       table.string('qr_data')
       table.string('qr_token_id')
       table.string('qr_token_status')
-      table.increments()
       table.timestamps()
     })
   }
